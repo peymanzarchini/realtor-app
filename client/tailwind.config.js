9d0afbc0ac97a142924f0195c86964ae73863369
peymanzarchini <peymanzarchini@outlook.com>
2023-10-08 02:05:44 +0330

    Adding fonts to the project and installing the required packages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black-rgba": "rgba(0,0,0,0.6)",
+        "black-rgba-light": "rgba(0,0,0,0.3)",
+        "footer-background": "rgb(43,43,43)",
+      },
+      backgroundImage: {
+        "Trend-image": "url('./src/assets/trends.webp')",
+      },
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
