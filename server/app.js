f5c51aa964c54309aaed6b0a9f59700dfae83114
peymanzarchini <peymanzarchini@outlook.com>
2023-10-08 03:45:26 +0330

    Initial structure of the project (backend)

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+import helmet from "helmet";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+//back-end security against malicious requests
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+//parser
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb" }));
+
+//static
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("server is running"));
