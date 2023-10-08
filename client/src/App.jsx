6c452cd413830fc5d4f8bec9913e37ad7f3a100a
peymanzarchini <peymanzarchini@outlook.com>
2023-10-08 03:36:24 +0330

    Creating a footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { HelmetProvider } from "react-helmet-async";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header/Header";
 import RoutesComponent from "./components/Routes";
+import Footer from "./components/Footer/Footer";
+import Copyright from "./components/Copyright/Copyright";
 
 const App = () => {
   const helmetContext = {};
@@ -13,8 +15,8 @@ const App = () => {
         <div>
           <RoutesComponent />
         </div>
-        {/* <Footer/> */}
-        {/* <Copyright/> */}
+        <Footer />
+        <Copyright />
         <ToastContainer
           position={"top-right"}
           autoClose={5000}
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import Container from "../Styles/Container";
+import FlexComponent from "../Styles/FlexComponent";
+import Logo from "../../assets/logo.svg";
+import { BsGithub, BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
+import Links from "../Links";
+import Store from "../Store";
+
+const Footer = () => {
+  return (
+    <footer className="w-[100%] bg-footer-background">
+      <Container classProps="py-10">
+        <div className="flex flex-col gap-10">
+          <FlexComponent classProps="flex-wrap gap-5 sm:flex-nowrap">
+            <div className="bg-white py-3 px-5 rounded-full">
+              <img src={Logo} alt="logo" className="w-[120px]" />
+            </div>
+            <div className="flex items-center gap-14">
+              <BsGithub
+                fontSize={30}
+                className="cursor-pointer rounded-full text-white hover:text-red-500"
+              />
+              <BsFacebook fontSize={30} className="cursor-pointer text-white hover:text-red-500" />
+              <BsInstagram fontSize={30} className="cursor-pointer text-white hover:text-red-500" />
+              <BsTwitter fontSize={30} className="cursor-pointer text-white hover:text-red-500" />
+            </div>
+          </FlexComponent>
+          <Links />
+          <Store />
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
