8fa5807e24e8fb64852745a9b65b362dd3bf9ca6
peymanzarchini <peymanzarchini@outlook.com>
2023-11-02 00:45:10 +0330

    Fix

--- server/middlewares/headers.js
+++ server/middlewares/headers.js
@@ -1,5 +1,5 @@
 export const setHeaders = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://realtor-estate.onrender.com");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   res.setHeader("Access-Control-Allow-Credentials", true);
