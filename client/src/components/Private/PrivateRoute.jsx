7af3d4d1d73d0ea21a6e58ed4875c1d470889039
Peyman Zarchini <peymanzarchini@outlook.com>
2024-05-04 02:18:31 +0330

    refactore code

--- client/src/components/Private/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoute = () => {
-  const user = useSelector((state) => state.user.currentUser);
-  return user ? <Outlet /> : <Navigate to="/sign-in" />;
-};
-
-export default PrivateRoute;
